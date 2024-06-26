41bbbac85ba05a59bdbb1dca9a98b0890601fcd0
youssef nagib <youssefnagib14@yahoo.com>
2024-06-26 13:40:08 +0300

    models

---
+++ models/engine/db_storage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" new class for sqlAlchemy """
+from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
+class DBStorage:
+    """ create tables in environmental"""
+    __engine = None
+    __session = None
+    def __init__(self):
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
